ab1e27bf65a4e9cd128f9533aa573a2e3e37b5cb
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-05 23:31:53 +0700

    update hw1-2

--- Homework/HW01/hw01-2-fix-bug.cpp
+++ Homework/HW01/hw01-2-fix-bug.cpp
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 int main() {
-
-    printf( "Hello I'm Javis (RMUTL-Version):\n" ) ;
-    scanf( "%d" ) ;
-    printf( "This is my first time to coding with C language\n" ) ;
-    printf( "Today is my birthday" ) ;
-
-    return 0 ;
-}//end main fucnion
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ;
+    printf( "This is my first time to coding with C language.\n" ) ;
+    printf( "Today is my birthday.\n" ) ;
+    return '0' ;
+}//end function
